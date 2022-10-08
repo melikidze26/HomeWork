3ce25dcb37ec14a3cbbf9b6721e415c3d6dccf69
vakhtang melikidze <mr.melikidze@gmail.com>
2022-10-08 19:45:07 +0400

    new visual progect

---
+++ Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your surname");
+
+            Console.ReadLine();
+
+        }
+    }
+}
